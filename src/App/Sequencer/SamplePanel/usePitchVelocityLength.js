ed6fa35d1a13612c5a2d05414a5ac931fe4698ee
nfcarbone <nfcarbone@gmail.com>
2021-04-02 11:59:51 -0400

    delete unneccessary effect in pvl

--- src/App/Sequencer/SamplePanel/usePitchVelocityLength.js
+++ src/App/Sequencer/SamplePanel/usePitchVelocityLength.js
@@ -8,7 +8,6 @@ import { MIDI_NOTES } from 'utils/MIDI_NOTES';
 export const usePitchVelocityLength = (mode) => {
   const dispatch = useDispatch();
   const selectedSample = useSelector((state) => state.editor.selectedSample);
-  // const value = useSelector((state) => state.editor.mods[mode]);
   const [value, setValue] = useState(
     mode === MODES.MOD_PITCH ? MIDI_NOTES[24] : 0.5
   );
@@ -28,6 +27,11 @@ export const usePitchVelocityLength = (mode) => {
     } else {
       setValue(Math.round(value * 100) / 100);
     }
+  };
+
+  // velocity & length
+  const onTouchEnd = () => {
+    if (editAll) dispatchModAll();
   };
 
   const dispatchModAll = () => {
@@ -50,22 +54,6 @@ export const usePitchVelocityLength = (mode) => {
       hideEditable();
     }
   };
-
-  // velocity & length
-  const onTouchEnd = () => {
-    if (editAll) dispatchModAll();
-  };
-
-  useEffect(() => {
-    if (mode === MODES.MOD_PITCH && editAll)
-      dispatch(
-        modAll({
-          selectedSample,
-          type: MODES.MOD_PITCH,
-          value,
-        })
-      );
-  }, [dispatch, editAll, mode, selectedSample, value]);
 
   const onReset = () => {
     dispatch(setModVal(INITIAL_MODS[mode]));
