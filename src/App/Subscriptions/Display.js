7043caab4207c205375dbaebc6ec9b48044c0cba
nfcarbone <nfcarbone@gmail.com>
2021-04-19 09:55:14 -0400

    testing named function in Display subscription for readability

--- src/App/Subscriptions/Display.js
+++ src/App/Subscriptions/Display.js
@@ -7,13 +7,32 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export const Display = () => {
   const dispatch = useDispatch();
-  const theme = useSelector((state) => state.app.theme);
   const splitSamplePanel = useSelector((state) => state.screen.splitSamplePanel);
   const transportStarted = useSelector((state) => state.tone.transportState === 'started');
   const analyzerOn = useSelector((state) => state.screen.analyzer.on);
   const editing = useSelector((state) => state.editor.selectedSample !== -1);
   const tapping = useSelector((state) => areWeTapping(state.editor.mode));
 
+  useHighlightSamplePanelIfTapping(tapping);
+  useTurnOnAnalyzerIfDeviceBigEnough(splitSamplePanel, analyzerOn, dispatch);
+  useHideAnalyzerWhileEditingOnSmallDevice(splitSamplePanel, analyzerOn, editing);
+  useStartAndStopAnalyzerAnimation(analyzerOn, tapping, transportStarted);
+  useFlashInfoWhenAnalyzerStops(dispatch, transportStarted);
+
+  useChangeTheme();
+
+  return null;
+};
+const useChangeTheme = () => {
+  const theme = useSelector((state) => state.app.theme);
+
+  useEffect(() => {
+    document.getElementById('root').className = `theme${theme}`;
+  }, [theme]);
+  return null;
+};
+
+const useHighlightSamplePanelIfTapping = (tapping) => {
   useEffect(() => {
     const spBorder = document.getElementById('sampleBtnsBorder');
     if (!spBorder) return;
@@ -21,12 +40,20 @@ export const Display = () => {
     if (!tapping) spBorder.classList.remove('highlight');
   }, [tapping]);
 
+  return null;
+};
+
+const useTurnOnAnalyzerIfDeviceBigEnough = (splitSamplePanel, analyzerOn, dispatch) => {
   useEffect(() => {
     if (splitSamplePanel && analyzerOn === null) {
       dispatch(setAnalyzerOn(true));
     }
   }, [splitSamplePanel, analyzerOn, dispatch]);
 
+  return null;
+};
+
+const useHideAnalyzerWhileEditingOnSmallDevice = (splitSamplePanel, analyzerOn, editing) => {
   useEffect(() => {
     if (splitSamplePanel || !analyzerOn) return;
     const analyzer = document.getElementById('analyzer');
@@ -35,22 +62,26 @@ export const Display = () => {
     if (editing) analyzer.style.opacity = 0;
   }, [analyzerOn, editing, splitSamplePanel]);
 
+  return null;
+};
+
+const useStartAndStopAnalyzerAnimation = (analyzerOn, transportStarted, tapping) => {
   useEffect(() => {
     if (!analyzerOn) return;
     if (transportStarted) startAnalyzer();
     else if (!transportStarted && !tapping) stopAnalyzer();
   }, [analyzerOn, tapping, transportStarted]);
 
+  return null;
+};
+
+const useFlashInfoWhenAnalyzerStops = (dispatch, transportStarted) => {
   useEffect(() => {
     if (!transportStarted) {
       dispatch(setFlashInfo(true));
       setTimeout(() => dispatch(setFlashInfo(false)), 0);
     }
   }, [dispatch, transportStarted]);
-
-  useEffect(() => {
-    document.getElementById('root').className = `theme${theme}`;
-  }, [theme]);
 
   return null;
 };
--- src/utils/network.js
+++ src/utils/network.js
@@ -1,7 +1,7 @@
 // export const HOST = 'http://localhost:4000';
-// export const HOST = 'http://192.168.1.234:4000';
+export const HOST = 'http://192.168.1.234:4000';
 // export const ORIGIN = 'http://localhost:3000';
-export const HOST = 'https://drumnickydrum-sequencer.herokuapp.com';
+// export const HOST = 'https://drumnickydrum-sequencer.herokuapp.com';
 export const ORIGIN = 'https://sequencer64.netlify.app';
 export const NETWORK_TIMEOUT = 5000;
 export const SHARE_TEXT = 'Check%20out%20my%20new%20beat%20using%20%23Sequencer64!%20';
