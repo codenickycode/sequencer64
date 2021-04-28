a6713d8500beb94b0a21f4702bedae374ff97783
nfcarbone <nfcarbone@gmail.com>
2021-04-28 10:40:11 -0400

    add path to setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    'sequencer/session/api',
     createProxyMiddleware({
       target: 'https://drumnickydrum-sequencer.herokuapp.com',
       changeOrigin: true,
