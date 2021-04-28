0341b0a36c3534760ac8ad581294d6d7ba53d148
nfcarbone <nfcarbone@gmail.com>
2021-04-28 10:21:18 -0400

    trying proxy to change origin for corse

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://drumnickydrum-sequencer.herokuapp.com/',
+      changeOrigin: true,
+    })
+  );
+};
