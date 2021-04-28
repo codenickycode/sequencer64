ded2f8e4479d9d01b39681d6bbdaf6b9226008fb
nfcarbone <nfcarbone@gmail.com>
2021-04-28 10:33:56 -0400

    fix host url /

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://drumnickydrum-sequencer.herokuapp.com/',
+      target: 'https://drumnickydrum-sequencer.herokuapp.com',
       changeOrigin: true,
     })
   );
--- src/utils/network.js
+++ src/utils/network.js
@@ -1,5 +1,5 @@
-export const HOST = 'api/';
-export const ORIGIN = 'https://sequencer64.netlify.app';
+export const HOST = 'api';
+export const ORIGIN = 'sequencer64.com';
 export const NETWORK_TIMEOUT = 5000;
 export const SHARE_TEXT = 'Check%20out%20my%20new%20beat%20using%20%23Sequencer64!%20';
 export const TWITTER_URL = `https://twitter.com/intent/tweet?text=${SHARE_TEXT}&url=`;
